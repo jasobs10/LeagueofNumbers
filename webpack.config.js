5d3303974762c434c984cc1e0d92e302cf3531b6
Jason Liu <jasobs10@gmail.com>
2017-05-08 01:20:07 -0400

    Add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,19 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
     },
+
+  module: {
+    loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }
+  ]
+},
   devtool: 'source-maps',
   resolve: {
     extensions: ["*", ".js"]
