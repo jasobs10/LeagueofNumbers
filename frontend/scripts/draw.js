6040b02b789bbc63dd9d8e82425e3b90dde3d855
Jason Liu <jasobs10@gmail.com>
2017-05-10 21:19:47 -0400

    Add changin axis

--- frontend/scripts/draw.js
+++ frontend/scripts/draw.js
@@ -64,7 +64,7 @@ class Draw {
       const xAx = $x.find(":selected").val();
       const yAx = $y.find(":selected").val();
       const options = {"x": xAx, "y": yAx};
-      this.render(options);
+      this.update(options);
     });
 
     $('button').click(this.handleClick.bind(this));
@@ -128,7 +128,7 @@ class Draw {
 
         }
 
-        this.render(options);
+        this.update(options);
       }
         i += 1;
       // } else {
@@ -232,15 +232,21 @@ class Draw {
 
     const x = d3.scaleLinear().range([0, width]);
     const y = d3.scaleLinear().range([height, 0]);
-    d3.select("svg").remove();
 
-    const svg = d3.select(".chart-container").append("svg")
-        // .attr("width", width + margin.left + margin.right)
-        // .attr("height", height + margin.bottom + margin.top)
-        .attr("width", 800)
-        .attr("height", 580)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    // d3.select("svg").remove();
+
+
+    // const svg = d3.select(".chart-container").append("svg")
+    //     // .attr("width", width + margin.left + margin.right)
+    //     // .attr("height", height + margin.bottom + margin.top)
+    //     .attr("width", 800)
+    //     .attr("height", 580)
+    //   .append("g")
+    //     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      const svg = d3.select("#main-g")
+        .selectAll('circle')
+        .data(this.list, (d) => d.stats[xKey]);
 
     d3.select(".tooltip").remove();
     const tooltip = d3.select(".chart-container").append("div")
@@ -278,11 +284,11 @@ class Draw {
     x.domain([xExtent[0] - (xRange * .1), xExtent[1] + (xRange * .1)]);
     y.domain([yExtent[0] - (yRange * .1), yExtent[1] + (yRange * .1)]);
 
-    svg.selectAll('dot')
-        // .data(this.list, (d) => {
-        //   //unique identifier of each piece of data
-        // });
-        .data(this.list)
+    svg.exit()
+      .remove();
+
+    svg
+        // .data(this.list)
       .enter().append('circle')
         .attr("r", (d) => {
 
@@ -305,60 +311,9 @@ class Draw {
         })
 
         .on("mouseover", (d) => {
-          const playerData = [];
-          const winData = [];
-          const damage = [];
-          // playerData.push({"name": d.name});
-          // playerData.push({"rank": d.rank});
-          // Object.keys(d.stats).forEach((key) => {
-          //   playerData.push({[key]: d.stats[key]});
-          //   // debugger
-          // });
-          playerData.push({"label": "Avg Kills", "value": d.stats.avgKills});
-          playerData.push({"label": "Avg Assists", "value": d.stats.avgAssists});
-          playerData.push({"label": "Avg Deaths", "value": d.stats.avgDeaths});
-
-          winData.push({"label": "Wins", "value": d.stats.totalSessionsWon});
-          winData.push({"label": "Losses", "value": d.stats.totalSessionsLost});
-
-          damage.push({"label": "Phys. Dmg", "value": d.stats.avgPhysicalDamage});
-          damage.push({"label": "Magic Dmg", "value": d.stats.avgMagicDamage});
-
-          // debugger
-          d3.select(".piesvg").remove();
-          d3.select(".piesvg").remove();
-          d3.select(".piesvg").remove();
-          // tooltip.html(`<h3>Summoner: ${d.name}</h3>` + `<article>${labels[xKey]}: ${d.stats[xKey]}</article>` + `<article>${labels[yKey]}: ${d.stats[yKey]}</article>`)
-
-          this.renderPie(playerData);
-          this.renderPie(winData);
-          this.renderPie(damage);
-
-          tooltip.transition()
-            .duration(200)
-            .style("opacity", .9)
-
-            //call draw function for new thing
-          tooltip.select(".tooltip-header")
-            .html(`<h3>Summoner: ${d.name}</h3>` + `<article>Rank: ${d.ranking}</article>` + `<article>${labels[xKey]}: ${d.stats[xKey]}</article>` + `<article>${labels[yKey]}: ${d.stats[yKey]}</article>`)
-          // tooltip.html(`<h3>Summoner: ${d.name}</h3>` + `<article>${labels[xKey]}: ${d.stats[xKey]}</article>` + `<article>${labels[yKey]}: ${d.stats[yKey]}</article>`)
-          // tooltip.exit().remove()
-          // tooltip.enter()
-              // .style("left", "45vw")
-              // .style("top", "10vh");
-          let xLocation;
-          if ((window.innerWidth - 520) < d3.event.pageX) {
-            xLocation = (d3.event.pageX - ($('.tooltip')[0].offsetWidth + 20));
-            // debugger
-          } else {
-            xLocation = (d3.event.pageX + 20);
-          }
-            tooltip
-              .style("left", (xLocation) + "px")
-              .style("top", (d3.event.pageY - 50) + "px");
 
 
-
+          this.handleHover(d, tooltip, xKey, yKey)
 
         })
         .on("mouseout", (d) => {
@@ -370,31 +325,37 @@ class Draw {
             // .remove();
         });
 
-    svg.append("g")
+    const gLine = d3.select("#main-g");
+
+    gLine.append("g")
         .attr("transform", "translate(0," + height + ")")
         .attr("class", "axis-line")
+        .attr("id", "axis-x")
         .call(d3.axisBottom(x)
           // .tickFormat(d3.format(".0s")));
           .ticks(15, "s"));
-    svg.append("text")
+    gLine.append("text")
       .attr("transform",
         // "translate(" + (width/2) + " ," + (height - margin.bottom + 60) + ")")
         "translate(357.5, 530)")
+      .attr("id", "x-text")
       .attr("dx", "1em")
       .style("text-anchor", "middle")
       .attr("class", "axis-label")
       .text(`${labels[xKey]}`);
 
-    svg.append("g")
+    gLine.append("g")
         .call(d3.axisLeft(y)
           .ticks(15, "s"))
 
           // .tickFormat(d3.format(".0s")))
-        .attr("class", "axis-line");
+        .attr("class", "axis-line")
+        .attr("id", "axis-y");
 
 
-    svg.append("text")
+    gLine.append("text")
       .attr("transform", "rotate(-90)")
+      .attr("id", "y-text")
       .attr("y", 0 - margin.left)
       .attr("x",0 - (height / 2))
       .attr("dy", "1em")
@@ -421,6 +382,140 @@ class Draw {
   hideError() {
     $('.error').hide();
   }
+
+  handleHover(d, tooltip, xKey, yKey) {
+    const playerData = [];
+    const winData = [];
+    const damage = [];
+    // playerData.push({"name": d.name});
+    // playerData.push({"rank": d.rank});
+    // Object.keys(d.stats).forEach((key) => {
+    //   playerData.push({[key]: d.stats[key]});
+    //   // debugger
+    // });
+    playerData.push({"label": "Avg Kills", "value": d.stats.avgKills});
+    playerData.push({"label": "Avg Assists", "value": d.stats.avgAssists});
+    playerData.push({"label": "Avg Deaths", "value": d.stats.avgDeaths});
+
+    winData.push({"label": "Wins", "value": d.stats.totalSessionsWon});
+    winData.push({"label": "Losses", "value": d.stats.totalSessionsLost});
+
+    damage.push({"label": "Phys. Dmg", "value": d.stats.avgPhysicalDamage});
+    damage.push({"label": "Magic Dmg", "value": d.stats.avgMagicDamage});
+
+    // debugger
+    d3.select(".piesvg").remove();
+    d3.select(".piesvg").remove();
+    d3.select(".piesvg").remove();
+    // tooltip.html(`<h3>Summoner: ${d.name}</h3>` + `<article>${labels[xKey]}: ${d.stats[xKey]}</article>` + `<article>${labels[yKey]}: ${d.stats[yKey]}</article>`)
+
+    this.renderPie(playerData);
+    this.renderPie(winData);
+    this.renderPie(damage);
+
+    tooltip.transition()
+      .duration(200)
+      .style("opacity", .9)
+
+      //call draw function for new thing
+    tooltip.select(".tooltip-header")
+      .html(`<h3>Summoner: ${d.name}</h3>` + `<article>Rank: ${d.ranking}</article>` + `<article>${labels[xKey]}: ${d.stats[xKey]}</article>` + `<article>${labels[yKey]}: ${d.stats[yKey]}</article>`)
+    // tooltip.html(`<h3>Summoner: ${d.name}</h3>` + `<article>${labels[xKey]}: ${d.stats[xKey]}</article>` + `<article>${labels[yKey]}: ${d.stats[yKey]}</article>`)
+    // tooltip.exit().remove()
+    // tooltip.enter()
+        // .style("left", "45vw")
+        // .style("top", "10vh");
+    let xLocation;
+    if ((window.innerWidth - 520) < d3.event.pageX) {
+      xLocation = (d3.event.pageX - ($('.tooltip')[0].offsetWidth + 20));
+      // debugger
+    } else {
+      xLocation = (d3.event.pageX + 20);
+    }
+      tooltip
+        .style("left", (xLocation) + "px")
+        .style("top", (d3.event.pageY - 50) + "px");
+
+  }
+
+  update(options) {
+    let xKey;
+    let yKey;
+    // debugger
+    const margin = {top: 30, right: 20, bottom: 30, left: 65},
+    width = 800 - margin.left - margin.right,
+    height = 550 - margin.top - margin.bottom;
+
+    xKey = options.x;
+    yKey = options.y;
+
+    $(`#x-axis option[value=${xKey}]`).prop('selected', true);
+    $(`#y-axis option[value=${yKey}]`).prop('selected', true);
+
+    const x = d3.scaleLinear().range([0, width]);
+    const y = d3.scaleLinear().range([height, 0]);
+
+
+    var t = d3.transition()
+      .duration(100)
+
+      this.list.forEach((d) => {
+        d.stats[xKey] = +d.stats[xKey];
+        d.stats[yKey] = +d.stats[yKey];
+        d.rank = +d.rank;
+      });
+
+      var xExtent = d3.extent(this.list, function(d) { return d.stats[xKey]; }),
+      xRange = xExtent[1] - xExtent[0],
+      yExtent = d3.extent(this.list, function(d) { return d.stats[yKey]; }),
+      yRange = yExtent[1] - yExtent[0];
+
+      // x.domain(d3.extent(this.list, (d) => d.stats[xKey]));
+      //
+      // y.domain([0, d3.max(this.list, (d) => d.stats[yKey])]);
+      x.domain([xExtent[0] - (xRange * .1), xExtent[1] + (xRange * .1)]);
+      y.domain([yExtent[0] - (yRange * .1), yExtent[1] + (yRange * .1)]);
+      d3.select(".tooltip").remove();
+      const tooltip = d3.select(".chart-container").append("div")
+          .attr("class", "tooltip")
+          .style("opacity", 0);
+
+      tooltip.append("div")
+          .attr("class", "tooltip-header");
+
+      tooltip.append("div")
+          .attr("class", "tooltip-chart");
+
+      const svg = d3.select("#main-g")
+        .selectAll('circle')
+        .data(this.list, (d) => d.stats[xKey]);
+
+        const xAxis = d3.select("#axis-x");
+        const yAxis = d3.select("#axis-y");
+        const xText = d3.select("#x-text");
+        const yText = d3.select("#y-text");
+
+        xAxis
+            .call(d3.axisBottom(x)
+              // .tickFormat(d3.format(".0s")));
+              .ticks(15, "s"));
+        xText
+          .text(`${labels[xKey]}`);
+
+        yAxis
+            .call(d3.axisLeft(y)
+              .ticks(15, "s"));
+
+              // .tickFormat(d3.format(".0s")))
+
+
+        yText
+          .text(`${labels[yKey]}`);
+
+          this.highlightClick();
+
+  }
+
 }
 
 
